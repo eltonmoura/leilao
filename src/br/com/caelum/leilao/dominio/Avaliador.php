77328dbe9c916c64a2649fa580113caa661c9e60
eltonmoura <elton.ms@gmail.com>
2017-12-04 18:14:43 -0200

    Corrigindo padrão de código

--- src/br/com/caelum/leilao/dominio/Avaliador.php
+++ src/br/com/caelum/leilao/dominio/Avaliador.php
@@ -3,56 +3,56 @@ namespace src\br\com\caelum\leilao\dominio;
 
 class Avaliador
 {
-	private $maiorValor = -INF;
-	private $menorValor = INF;
-	private $valorMedio;
-	
-	public function avalia(Leilao $leilao)
-	{
-	    if (empty($leilao->getLances())) {
-	        $this->menorValor = 0;
-	        $this->maiorValor = 0;
-	        $this->valorMedio = 0;
-	        return true;
-	    }
+    private $maiorValor = -INF;
+    private $menorValor = INF;
+    private $valorMedio;
+    
+    public function avalia(Leilao $leilao)
+    {
+        if (empty($leilao->getLances())) {
+            $this->menorValor = 0;
+            $this->maiorValor = 0;
+            $this->valorMedio = 0;
+            return true;
+        }
 
-	    $valores = [];
-	    $total = 0;
-	    foreach ($leilao->getLances() as $lance) {
-	        $valores[] = $lance->getValor();
-	        
-			if ($lance->getValor() < $this->menorValor) {
-				$this->menorValor = $lance->getValor();
-			}
-			if ($lance->getValor() > $this->maiorValor) {
-				$this->maiorValor = $lance->getValor();
-			}
-			$total += $lance->getValor();
-		}
+        $valores = [];
+        $total = 0;
+        foreach ($leilao->getLances() as $lance) {
+            $valores[] = $lance->getValor();
+            
+            if ($lance->getValor() < $this->menorValor) {
+                $this->menorValor = $lance->getValor();
+            }
+            if ($lance->getValor() > $this->maiorValor) {
+                $this->maiorValor = $lance->getValor();
+            }
+            $total += $lance->getValor();
+        }
 
-		rsort($valores);
-		$this->tresMaiores = array_chunk($valores, 3)[0];
+        rsort($valores);
+        $this->tresMaiores = array_chunk($valores, 3)[0];
 
-		$this->valorMedio = ($total / count($leilao->getLances()));
-	}
+        $this->valorMedio = ($total / count($leilao->getLances()));
+    }
 
-	public function getValorMedio()
-	{
-	    return $this->valorMedio;
-	}
-	
-	public function getTresMaiores()
-	{
-	    return $this->tresMaiores;
-	}
+    public function getValorMedio()
+    {
+        return $this->valorMedio;
+    }
+    
+    public function getTresMaiores()
+    {
+        return $this->tresMaiores;
+    }
 
-	public function getMaiorValor()
-	{
-		return $this->maiorValor;
-	}
+    public function getMaiorValor()
+    {
+        return $this->maiorValor;
+    }
 
-	public function getMenorValor()
-	{
-		return $this->menorValor;
-	}	
+    public function getMenorValor()
+    {
+        return $this->menorValor;
+    }   
 }
